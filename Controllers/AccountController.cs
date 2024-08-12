1ecb30c80b80eb3fe92dc0b9e93b86e032522c1c
Harla_oq45@student.itstep.org <Harla_oq45@student.itstep.org>
2024-08-12 19:12:04 +0200

    Добавьте файлы проекта.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using LoginDemo.Models;
+
+namespace LoginDemo.Controllers
+{
+    public class AccountController : Controller
+    {
+        private const string UsernameCookie = "username";
+        private static readonly User ValidUser = new User { Username = "admin", Password = "password" };
+
+        [HttpGet]
+        public IActionResult Login(string errorMessage = "")
+        {
+            ViewBag.ErrorMessage = errorMessage;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string password)
+        {
+            if (username != ValidUser.Username)
+            {
+                return RedirectToAction("Login", new { errorMessage = "Користувач не існує" });
+            }
+            if (password != ValidUser.Password)
+            {
+                return RedirectToAction("Login", new { errorMessage = "Пароль не вірний" });
+            }
+
+            // кукі з ім'ям користувача
+            Response.Cookies.Append(UsernameCookie, username, new CookieOptions { HttpOnly = true });
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Logout()
+        {
+            // Видаляємо кукі
+            Response.Cookies.Delete(UsernameCookie);
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LoginDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private const string UsernameCookie = "username";
+
+        public IActionResult Index()
+        {
+            // Перевірка наявності кукі
+            if (!Request.Cookies.ContainsKey(UsernameCookie))
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            string username = Request.Cookies[UsernameCookie];
+            ViewBag.Username = username;
+            return View();
+        }
+    }
+}
